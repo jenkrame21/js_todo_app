7d9e90c6ded06f68e0af6832dad9163c21cc4918
Jennifer Kramer <jenkrame@gmail.com>
2021-06-18 15:07:25 -0400

    Creatd useEffects for local storage of todos. Updated h1 tag.

--- js-todo-app/src/App.js
+++ js-todo-app/src/App.js
@@ -1,17 +1,43 @@
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./components/FontAwesomeIcons/index";
 
 function App() {
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
 
+  // Runs getLocalTodos once page loads
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  // Saves todos to local storage
+  useEffect(() => {
+    saveLocalTodos(todos);
+  });
+
+  // Grabs todos created in local storage
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  };
+
+  // Sets todos in local storage
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+
   return (
     <div className="App">
-      <h1>My Todo App</h1>
-      <TodoForm inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos}/>
+      <h1>What Todos Will You Get Done Today?</h1>
+      <TodoForm inputText={inputText} setInputText={setInputText} todos={todos} setTodos={setTodos} saveLocalTodos={saveLocalTodos}/>
       <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
